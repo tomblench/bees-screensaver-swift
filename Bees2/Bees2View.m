8c80bfa238423a978d217e6bf35785b2bb86fa59
Tom Blench <tom@blench.org>
2018-04-09 20:28:44 +0100

    Initial Commit

---
+++ Bees2/Bees2View.m
@@ -0,0 +1,52 @@
+//
+//  Bees2View.m
+//  Bees2
+//
+//  Created by Thomas Blench on 09/04/2018.
+//  Copyright © 2018 Thomas Blench. All rights reserved.
+//
+
+#import "Bees2View.h"
+
+@implementation Bees2View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
