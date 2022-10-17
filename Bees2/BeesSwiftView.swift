89559e848b7ff1835ee7870a0d5a14d898a57146
Tom Blench <tom@blench.org>
2022-10-17 18:47:19 +0100

    rainbow swarm; more preferences

--- Bees2/BeesSwiftView.swift
+++ Bees2/BeesSwiftView.swift
@@ -12,8 +12,8 @@ import ScreenSaver
 public class BeesSwiftView: ScreenSaverView {
 
     // prefs
-    @IBOutlet weak var queenNumberSlider: NSSlider!
-    @IBOutlet weak var swarmNumberSlider: NSSlider!
+    @IBOutlet weak var queenNumberSlider: NSTextField!
+    @IBOutlet weak var swarmNumberSlider: NSTextField!
     @IBOutlet weak var queenSpeedSlider: NSSlider!
     @IBOutlet weak var swarmSpeedSlider: NSSlider!
     @IBOutlet weak var swarmAccelerationSlider: NSSlider!
@@ -21,6 +21,8 @@ public class BeesSwiftView: ScreenSaverView {
     @IBOutlet weak var fadeSlider: NSSlider!
     @IBOutlet weak var queenColorWell: NSColorWell!
     @IBOutlet weak var swarmColorWell: NSColorWell!
+    @IBOutlet weak var swarmRainbowCheckbox: NSButton!
+    @IBOutlet weak var queenVisibleCheckbox: NSButton!
     static let queenNumberPrefsKey = "queenNumber"
     static let swarmNumberPrefsKey = "swarmNumber"
     static let queenSpeedPrefsKey = "queenSpeed"
@@ -29,7 +31,9 @@ public class BeesSwiftView: ScreenSaverView {
     static let swarmAccelerationPrefsKey = "swarmAcceleration"
     static let fadePrefsKey = "fade"
     static let queenColourPrefsKey = "queenColour"
+    static let queenVisiblePrefsKey = "queenVisible"
     static let swarmColourPrefsKey = "swarmColour"
+    static let swarmRainbowPrefsKey = "swarmRainbow"
     var saverDefaults: ScreenSaverDefaults?
 
     // beezzz
@@ -59,6 +63,30 @@ public class BeesSwiftView: ScreenSaverView {
         super.init(frame: frame, isPreview: isPreview)
         // use our bundle id to register defaults
         NSLog("Bundle name %@", Bundle.init(for: BeesSwiftView.self).bundleIdentifier!)
+        // TODO update defaults for all keys
+        /*
+         Saver defaults <ScreenSaverDefaults: 0x133e265f0>
+         {
+             fade = "0.06514469844357977";
+             queenColour =     (
+                 1,
+                 1,
+                 1,
+                 1
+             );
+             queenNumber = 10;
+             queenSpeed = "45.62776820866141";
+             swarmAcceleration = "0.00500738188976378";
+             swarmColour =     (
+                 "0.5",
+                 "0.6182755878741468",
+                 1
+             );
+             swarmNumber = 1000;
+             swarmRespawnRadius = 20;
+             swarmSpeed = "15.33126230314961";
+         }
+         */
         let defaults = [BeesSwiftView.queenNumberPrefsKey:1,
                         BeesSwiftView.swarmNumberPrefsKey:10,
                         BeesSwiftView.queenSpeedPrefsKey:5.710116731517509,
@@ -94,26 +122,19 @@ public class BeesSwiftView: ScreenSaverView {
     }
 
     public override func draw(_ dirtyRect: NSRect) {
-        NSLog("in draw")
         super.draw(dirtyRect)
-//            NSLog("get buffer")
             
         let buffer = CGLayer(NSGraphicsContext.current!.cgContext, size: frame.size, auxiliaryInfo: nil)
-//            NSLog("got buffer")
 
-//        let context = NSGraphicsContext.current?.cgContext
         let context = buffer?.context
 
         if (self.image != nil) {
             buffer?.context!.draw(image!, in: self.bounds)
         }
 
-        context?.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.01)
+        context?.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.1)
         context?.fill(self.bounds)
 
-//        NSLog("context %@", context.debugDescription)
-        
-//        NSLog("buffer %@", buffer.debugDescription)
 
         // animate swarm
         let colour = saverDefaults?.array(forKey: BeesSwiftView.swarmColourPrefsKey) as! [Double]
@@ -123,10 +144,12 @@ public class BeesSwiftView: ScreenSaverView {
         let queenSpeed = (saverDefaults?.float(forKey: BeesSwiftView.queenSpeedPrefsKey))!
         let swarmSpeed = (saverDefaults?.float(forKey: BeesSwiftView.swarmSpeedPrefsKey))!
         let respawnRadius² = respawnRadius*respawnRadius
+        let queenVisible = (saverDefaults?.bool(forKey: BeesSwiftView.queenVisiblePrefsKey))!
+        let swarmRainbow = (saverDefaults?.bool(forKey: BeesSwiftView.swarmRainbowPrefsKey))!
         context?.setBlendMode(CGBlendMode.lighten)
         
         context?.setLineWidth(5.0)
-        context?.setLineCap(CGLineCap.round)
+        context?.setLineCap(CGLineCap.butt)
         for d in swarm {
             // set vector towards queen
             // find closest queen, its difference vector, and magnitude
@@ -147,13 +170,22 @@ public class BeesSwiftView: ScreenSaverView {
             // change direction vector to seek queen
             d.direction = d.direction + (diff?.1)! * swarmAcceleration
             // limit speed of drones
-//            let scale = d.direction.mag²() / (swarmSpeed*swarmSpeed)
             let scale = d.direction.mag() / swarmSpeed
             if (scale > 1.0) {
                 d.direction = d.direction / scale
             }
             // fade in by age
-            context?.setStrokeColor(red: CGFloat(colour[0]), green: CGFloat(colour[1]), blue: CGFloat(colour[2]), alpha: CGFloat(min(Double(d.age) / 300.0, 1.0)))
+            
+            let col = hsvToRgb(h: Float(d.age) / Float(BeesSwiftView.ageLimit), s: 1.0, v: 1.0)
+            
+            
+            if (swarmRainbow) {
+                context?.setStrokeColor(red: CGFloat(col.0), green: CGFloat(col.1), blue: CGFloat(col.2), alpha: CGFloat(min(Double(d.age) / 300.0, 1.0)))
+
+            } else {
+                context?.setStrokeColor(red: CGFloat(colour[0]), green: CGFloat(colour[1]), blue: CGFloat(colour[2]), alpha: CGFloat(min(Double(d.age) / 300.0, 1.0)))
+            }
+            
             context?.strokeLineSegments(between: [CGPoint(x: Double(d.position.x), y: Double(d.position.y)),
                                                   CGPoint(x: Double(d.position.x + d.direction.x), y: Double(d.position.y+d.direction.y))])
             d.step()
@@ -176,9 +208,11 @@ public class BeesSwiftView: ScreenSaverView {
             else if (q.position.y > Float(self.bounds.height)) {
                 q.position.y = 0
             }
-            context?.setStrokeColor(red: CGFloat(queenColour[0]), green: CGFloat(queenColour[1]), blue: CGFloat(queenColour[2]), alpha: CGFloat(queenColour[3]))
-            context?.strokeLineSegments(between: [CGPoint(x: Double(q.position.x), y: Double(q.position.y)),
-                                                  CGPoint(x: Double(q.position.x + q.direction.x), y: Double(q.position.y+q.direction.y))])
+            if (queenVisible) {
+                context?.setStrokeColor(red: CGFloat(queenColour[0]), green: CGFloat(queenColour[1]), blue: CGFloat(queenColour[2]), alpha: CGFloat(queenColour[3]))
+                context?.strokeLineSegments(between: [CGPoint(x: Double(q.position.x), y: Double(q.position.y)),
+                                                      CGPoint(x: Double(q.position.x + q.direction.x), y: Double(q.position.y+q.direction.y))])
+            }
             q.step()
 
         }
@@ -215,7 +249,12 @@ public class BeesSwiftView: ScreenSaverView {
             self.swarmColorWell.color = NSColor(red: CGFloat(colour[0]), green: CGFloat(colour[1]), blue: CGFloat(colour[2]), alpha: 1.0)
             let queenColour = (saverDefaults?.array(forKey: BeesSwiftView.queenColourPrefsKey)) as! [Double]
             self.queenColorWell.color = NSColor(red: CGFloat(queenColour[0]), green: CGFloat(queenColour[1]), blue: CGFloat(queenColour[2]), alpha: CGFloat(queenColour[3]))
+            self.queenVisibleCheckbox.state = (saverDefaults?.bool(forKey: BeesSwiftView.queenVisiblePrefsKey))! == true ? NSControl.StateValue.on : NSControl.StateValue.off
+            self.swarmRainbowCheckbox.state = (saverDefaults?.bool(forKey: BeesSwiftView.swarmRainbowPrefsKey))! == true ? NSControl.StateValue.on : NSControl.StateValue.off
             NSLog("**** configureSheet with %@", prefsWindow ?? "nil")
+                
+            onQueenVisibleToggle(Void.self)
+            onSwarmRainbowToggle(Void.self)
             return prefsWindow
         }
     }
@@ -231,11 +270,73 @@ public class BeesSwiftView: ScreenSaverView {
         saverDefaults?.set(fadeSlider.doubleValue, forKey: BeesSwiftView.fadePrefsKey)
         saverDefaults?.set([swarmColorWell.color.redComponent.native, swarmColorWell.color.greenComponent.native, swarmColorWell.color.blueComponent.native], forKey: BeesSwiftView.swarmColourPrefsKey)
         saverDefaults?.set([queenColorWell.color.redComponent.native, queenColorWell.color.greenComponent.native, queenColorWell.color.blueComponent.native, queenColorWell.color.alphaComponent.native], forKey: BeesSwiftView.queenColourPrefsKey)
+        saverDefaults?.set(queenVisibleCheckbox.state == NSControl.StateValue.on ? true : false, forKey: BeesSwiftView.queenVisiblePrefsKey)
+        saverDefaults?.set(swarmRainbowCheckbox.state == NSControl.StateValue.on ? true : false, forKey: BeesSwiftView.swarmRainbowPrefsKey)
+
         NSColorPanel.shared.orderOut(self)
         // TODO figure out how to replace this deprecated method
         NSLog("on ok Saver defaults %@", saverDefaults ?? "nil")
         saverDefaults?.synchronize()
         NSApp.endSheet(prefsWindow!)
+    }
+    
+    @IBAction func onQueenVisibleToggle(_ sender: Any) {
+        queenColorWell.isEnabled = queenVisibleCheckbox.state == NSControl.StateValue.on ? true : false
+    }
+
+    @IBAction func onSwarmRainbowToggle(_ sender: Any) {
+        swarmColorWell.isEnabled = swarmRainbowCheckbox.state == NSControl.StateValue.on ? true : false
+    }
+
+    
+    func hsvToRgb(h: Float, s: Float, v: Float) -> (Float, Float, Float)
+    {
+
+
+        let hh = h * 6.0
+        let i = Int(hh)
+        let ff = hh - Float(i)
+        let p = v * (1.0 - s)
+        let q = v * (1.0 - (s * ff))
+        let t = v * (1.0 - (s * (1.0 - ff)))
+        var r = Float()
+        var g = Float()
+        var b = Float()
+        switch(i) {
+            case 0:
+                r = v;
+                g = t;
+                b = p;
+                break;
+            case 1:
+                r = q;
+                g = v;
+                b = p;
+                break;
+            case 2:
+                r = p;
+                g = v;
+                b = t;
+                break;
+
+            case 3:
+                r = p;
+                g = q;
+                b = v;
+                break;
+            case 4:
+                r = t;
+                g = p;
+                b = v;
+                break;
+            default:
+                r = v;
+                g = p;
+                b = q;
+                break;
+            }
+
+        return (r, g, b)
     }
     
 }
